17a61d17609220d41bd542edc5338a8a2b0fbb6d
mguel9214 <iguel921433@gmail.com>
2023-07-27 11:26:19 -0500

    funciones de slmacer y eliminar configuradas

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome',[
+        'tags'=> App\Models\Tag::get()
+    ]);
+});
+
+Route::post('tags',[App\Http\Controllers\TagController::class,'store']);
+
+Route::delete('tags/{tag}',[App\Http\Controllers\TagController::class,'destroy']);
+
+// Route::post('tags/',[App\Http\Controllers\TagController::class,'validate']);
+
+
